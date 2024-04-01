4f4433a030aabcebe43f6f8081fe5c2eec4c86e7
primedev88 <anujk7.dev@gmail.com>
2024-04-01 16:05:05 +0530

    committed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import styles from './Home.module.css'
+import '@fontsource/inter'; // Import Inter font
 
 const Home = () => {
+  
   return (
-    <div>
-      Home
+    <div className={styles.home}>
+      <div className={styles.wifibody}>
+        <div className={styles.wificount}>
+          Wifi Nodes : 0
+        </div>
+        <div className={styles.wifidevice}>
+          Try connecting a device
+        </div>
+      </div>
+      <div className={styles.lorabody}>
+        <div className={styles.loracount}>
+          Lora Nodes : 0
+        </div>
+        <div className={styles.loradevice}>
+          Try connecting a device
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,57 +1,65 @@
 // src/Navbar.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from './Navbar.module.css'
-import { MdOutlineSignalCellularAlt,MdOutlineSpaceDashboard ,MdOutlineSettings } from "react-icons/md";
+import { MdOutlineSignalCellularAlt, MdOutlineSpaceDashboard, MdOutlineSettings } from "react-icons/md";
 import { FaDatabase } from "react-icons/fa";
 import { LuDroplets } from "react-icons/lu";
 import { RiSignalTowerFill } from "react-icons/ri";
 import { FiDatabase } from "react-icons/fi";
 
 const Navbar = () => {
+  const router = useRouter();
+
+  const isActiveLink = (href) =>{
+    return router.pathname ===href;
+  }
   
   return (
 
     <nav className={styles.navbar}>
       <div >
-        <img src="/static/favicon.ico" alt="Logo"  />
+        <img src="/static/favicon.ico" alt="Logo" />
       </div>
       <div className={styles.flexGrow}>
         <div className={styles.connect}>
-          <MdOutlineSignalCellularAlt style={{fontSize:'25'}}/>
+          <MdOutlineSignalCellularAlt style={{ fontSize: '25' }} />
         </div>
         <ul className={styles.selectul}>
           <li className={styles.selectli}>
             <Link legacyBehavior href="/" >
               <a className={styles.link}>
-                <MdOutlineSpaceDashboard style={{fontSize:'26',marginTop:'3vh'}}/>
-                <div style={{width:'100%', background:'white', height: '1vh', marginTop: '1.5vh'}}></div>
+                <MdOutlineSpaceDashboard style={{ fontSize: '26', marginTop: '3vh' ,color:isActiveLink('/')? '#60F9A6':'white' }} />
+                <div style={{ width: '100%', background:  isActiveLink('/')? '#60F9A6':'#03334E', height: '1vh', marginTop: '1.5vh', borderRadius: '4px' }}></div>
               </a>
             </Link>
           </li>
           <li className={styles.selectli}>
             <Link legacyBehavior href="/data">
-            <a className={styles.link}>
-              <FiDatabase  style={{fontSize:'24',marginTop:'3vh'}}/>
-            </a>
+              <a className={styles.link} >
+                <FiDatabase style={{ fontSize: '24', marginTop: '3vh' , color:isActiveLink('/data')? '#60F9A6':'white' }} />
+                <div style={{ width: '100%', background:  isActiveLink('/data')? '#60F9A6':'#03334E', height: '1vh', marginTop: '1.5vh', borderRadius: '4px' }}></div>
+              </a>
             </Link>
           </li>
-          <li className={styles.selectli}>
+          <li className={styles.selectli} >
             <Link legacyBehavior href="/about">
-            <a className={styles.link}>
-              <LuDroplets style={{fontSize:'24',marginTop:'3vh'}}/>
-            </a>
+              <a className={styles.link} >
+                <LuDroplets style={{ fontSize: '24', marginTop: '3vh' ,color:isActiveLink('/about')? '#60F9A6':'white' }} />
+                <div style={{ width: '100%', background:  isActiveLink('/about')? '#60F9A6':'#03334E', height: '1vh', marginTop: '1.5vh', borderRadius: '4px' }}></div>
+              </a>
             </Link>
           </li>
-          
+
         </ul>
       </div>
       <div className={styles.setting}>
         <div className={styles.hotspot}>
-          <RiSignalTowerFill style={{fontSize:'23'}}/>
+          <RiSignalTowerFill style={{ fontSize: '23' }} />
         </div>
         <div className={styles.setting2}>
-          <MdOutlineSettings style={{fontSize:'24'}}/>
+          <MdOutlineSettings style={{ fontSize: '24' }} />
         </div>
       </div>
     </nav>
