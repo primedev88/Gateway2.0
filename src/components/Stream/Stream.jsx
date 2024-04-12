35a6e7da4ea58c357591c75195f234d217844dd4
primedev88 <anujk7.dev@gmail.com>
2024-04-12 18:33:13 +0530

    comm

--- src/components/Stream/Stream.jsx
+++ src/components/Stream/Stream.jsx
@@ -48,11 +48,6 @@ const Stream = () => {
         }
     }
 
-    // Function to generate a random string for cache busting
-    const generateCacheBustingParam = () => {
-        return `?cache=${Math.random().toString(36).substring(7)}`;
-    }
-
     return (
         <div className={styles.stream}>
             <div className={styles.button} onClick={handleStream} disable={isLoadingStream.toString()}>
@@ -62,7 +57,7 @@ const Stream = () => {
             </div>
             <div className={styles.streamWindow}>
                 {stream && (
-                    <img style={{display:'block',margin:'auto'}} src={`http://localhost:8084/?action=stream${generateCacheBustingParam()}`} width="440" height="250"/>
+                    <img style={{display:'block',margin:'auto'}} src={`http://localhost:8084/?action=stream`} width="440" height="250"/>
                 )}
             </div>
         </div>
